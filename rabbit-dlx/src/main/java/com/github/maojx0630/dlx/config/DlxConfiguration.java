d621192082163febe51c9e05a12309aee3f3c2f3
毛家兴 <908156949@qq.com>
2019-05-23 18:05:23 +0800

    init

--- rabbit-dlx/src/main/java/com/github/maojx0630/dlx/config/DlxConfiguration.java
+++ rabbit-dlx/src/main/java/com/github/maojx0630/dlx/config/DlxConfiguration.java
@@ -18,10 +18,11 @@ public class DlxConfiguration {
 	public static final String DEAD_LETTER_EXCHANGE = "TDL_EXCHANGE";
 
 
-	public static final String DEAD_LETTER_TEST_ROUTING_KEY = "TDL_KEY";
-	public static final String DEAD_LETTER_REDIRECT_ROUTING_KEY = "TKEY_R";
 	public static final String DEAD_LETTER_QUEUE = "TDL_QUEUE";
 	public static final String REDIRECT_QUEUE = "TREDIRECT_QUEUE";
+
+	public static final String DEAD_LETTER_TEST_ROUTING_KEY = "TDL_KEY";
+	public static final String DEAD_LETTER_REDIRECT_ROUTING_KEY = "TKEY_R";
 
 	/**
 	 * 死信队列跟交换机类型没有关系 不一定为directExchange  不影响该类型交换机的特性.
