03407e8e7e2919ebd9cb6bb22d6d7f8a162b3eb5
毛家兴 <908156949@qq.com>
2019-05-23 16:53:57 +0800

    init

---
+++ rabbit-dlx/src/main/java/com/github/maojx0630/dlx/DlxApplication.java
@@ -0,0 +1,17 @@
+package com.github.maojx0630.dlx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: MaoJiaXing
+ * @date: 2019-05-23 16:50
+ * @description:
+ */
+@SpringBootApplication
+public class DlxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlxApplication.class);
+	}
+}
